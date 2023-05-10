5b3b4ca60dd3d32ef8fa4b80e9e6ce86b65f060e
Gildomiro Bairros <gbairros@gmail.com>
2023-05-09 21:40:14 -0300

    Paths

--- infra/autoload.php
+++ infra/autoload.php
@@ -3,10 +3,12 @@ spl_autoload_register(function ($class_name) {
     require_once __DIR__."/".$class_name . '.php';
 });
 
+
+
 $obj  = new Database();
 $obj->teste();
-echo "> ".__DIR__."\n";
-echo "<br>";
-echo $_SERVER["DOCUMENT_ROOT"];
+
+
+
 echo "<br>";
 ?>
